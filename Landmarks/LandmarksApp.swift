f7a72535178116cc9185f6a753fa9b2d617a2d55
Katie Saramutina <saramutina@gmail.com>
2023-05-09 19:06:46 +0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Katie Saramutina on 09.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
