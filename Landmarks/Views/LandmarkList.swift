1cd40e890e7c769ec1f287fa777e53f52c05be2f
Katie Saramutina <saramutina@gmail.com>
2023-05-23 00:44:30 +0400

    ✨Add landmarks list and data
    Clicking on a landmark in a list opens detailed info about each landmark

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,33 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Katie Saramutina on 23.05.2023.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (3rd generation)", "iPhone 14 Pro Max", "iPad Pro (12.9-inch) (6th generation)"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
