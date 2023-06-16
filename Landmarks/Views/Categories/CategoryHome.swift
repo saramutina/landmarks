22c1c23e53d2b605dd1f4ef4b388c3a11d1bf2be
Katie Saramutina <saramutina@gmail.com>
2023-06-16 08:06:45 +0400

    ✨ Add profile view with sample data
    Clicking on icon in the top right corner of Featured (main) view opens profile

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
+    @State private var showingProfile = false
     
     var body: some View {
         NavigationView {
@@ -25,8 +26,19 @@ struct CategoryHome: View {
                         .listRowInsets(EdgeInsets())
                 }
             }
-            .listStyle(.plain)
+            .listStyle(.inset)
             .navigationTitle("Featured")
+            .toolbar {
+                Button(action: {
+                    showingProfile.toggle()
+                }, label: {
+                    Label("User Profile", systemImage: "person.crop.circle")
+                })
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environmentObject(modelData)
+            }
         }
     }
 }
---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Katie Saramutina on 16.06.2023.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
